1cf9317b251262b35374d83b09ecfaa4bf5888b7
Fernando <pinho.dcj@gmail.com>
2019-03-26 14:47:48 +0100

    first commit

---
+++ js/paris.js
@@ -0,0 +1,8 @@
+//LA METEO
+	$.getJSON("https://api.openweathermap.org/data/2.5/weather?q=paris,fr&units=metric&appid=2ebe19fb2e7dc8831190daf8260d7761", function (response){
+			var temperature = response.main.temp;
+			// alert("Il fait actuellement "+temperature+"°C à Rome !");
+			var changeTexteTemp = document.getElementById("temperature");
+			changeTexteTemp.innerHTML = "Il fait actuellement "+temperature+"°C à Paris !";
+			console.log(temperature);
+	});
